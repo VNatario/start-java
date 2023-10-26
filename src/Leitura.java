d2a000f3d634c886855b5009d42d657507217f91
Vitor Natario <natariovitor@gmail.com>
2023-10-26 18:04:57 -0300

    feat: keyboard input

---
+++ src/Leitura.java
@@ -0,0 +1,11 @@
+import java.util.Scanner;
+
+public class Leitura {
+    public static void main(String[] args) {
+        Scanner leitura = new Scanner(System.in);
+
+        System.out.println("Digite seu filme favorito");
+        String filme = leitura.nextLine();
+        System.out.println(filme);
+    }
+}
